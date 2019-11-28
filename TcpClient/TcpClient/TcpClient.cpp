1bf255335cf63732a57fa5b84b3ecb8af85a8f95
yangquanyu <yangquanyu@qq.com>
2019-11-29 00:09:33 +0800

    简单客户端测试代码。

--- TcpClient/TcpClient/TcpClient.cpp
+++ TcpClient/TcpClient/TcpClient.cpp
@@ -5,50 +5,37 @@
 #include <iostream>
 #include <Winsock2.h>
 #include <stdio.h>
+#include <string>
 #pragma comment(lib,"ws2_32.lib")
 
-void main()
+int main()
 {
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
-	wVersionRequested = MAKEWORD(1, 1);
-
-	err = WSAStartup(wVersionRequested, &wsaData);
-	if (err != 0) {
-		return;
+	WSADATA wsaData = { 0 };
+	int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (err != 0) 
+	{
+		return 1;
 	}
 
-
-	if (LOBYTE(wsaData.wVersion) != 1 ||
-		HIBYTE(wsaData.wVersion) != 1) {
-		WSACleanup();
-		return;
-	}
-	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	SOCKADDR_IN addrSrv;
 	addrSrv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 	addrSrv.sin_family = AF_INET;
-	addrSrv.sin_port = htons(6000);
+	addrSrv.sin_port = htons(8866);
 	connect(sockClient, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
 
-	char recvBuf[100];
-	recv(sockClient, recvBuf, 100, 0);
-	printf("%s\n", recvBuf);
-	send(sockClient, "This is lisi", strlen("This is lisi") + 1, 0);
+	std::string sendData = "I coming!!!!!";
+	int nRet = send(sockClient, sendData.c_str(), sendData.size(), 0);
+
+	char recvBuf[256] = { 0 };
+	nRet = recv(sockClient, recvBuf, 256, 0);
+	if (nRet > 0)
+	{
+		recvBuf[nRet] = '\0';
+		std::cout << "Recv server data=" << recvBuf << std::endl;
+	}
 
 	closesocket(sockClient);
 	WSACleanup();
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
