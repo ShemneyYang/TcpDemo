47e5ba00ad2863b752744296b4506c77241d64c5
yangquanyu <yangquanyu@qq.com>
2019-11-28 23:56:29 +0800

    增加第一版本代码。

--- TcpServer/TcpServer/TcpServer.cpp
+++ TcpServer/TcpServer/TcpServer.cpp
@@ -5,64 +5,101 @@
 #include <iostream>
 #include <Winsock2.h>
 #include <stdio.h>
+#include <set>
+#include <string>
+#include <Windows.h>
+#include <process.h>
+
 #pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+struct SThreadInfo
+{
+	SOCKET socket;
+	std::string strIP;
+};
+
+unsigned __stdcall threadProc(LPVOID lpParam);
 
 int main()
 {
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
-	wVersionRequested = MAKEWORD(1, 1);
-
-	err = WSAStartup(wVersionRequested, &wsaData);
-	if (err != 0) {
-		return -1;
+	std::cout << "Start TCP Server!!!!!!!!" << endl;
+	WSADATA wsaData = { 0 };
+	int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (err != NO_ERROR) 
+	{
+		std::cout << "Error!! Failed to WSAStartup, err=" << WSAGetLastError() << endl;
+		return 1;
 	}
 
-
-	if (LOBYTE(wsaData.wVersion) != 1 ||
-		HIBYTE(wsaData.wVersion) != 1) {
-		WSACleanup();
-		return -1;
-	}
-	SOCKET sockSrv = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET sockSrv = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	SOCKADDR_IN addrSrv;
 	addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
 	addrSrv.sin_family = AF_INET;
 	addrSrv.sin_port = htons(8866);
 
-	bind(sockSrv, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
+	if (SOCKET_ERROR == bind(sockSrv, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR)))
+	{
+		std::cout << "Error!! Failed to bind, err=" << WSAGetLastError() << endl;
+		return 1;
+	}
 
-	listen(sockSrv, 5);
+	if (SOCKET_ERROR == listen(sockSrv, 5))
+	{
+		std::cout << "Error!! Failed to listen, err=" << WSAGetLastError() << endl;
+		return 1;
+	}
 
 	SOCKADDR_IN addrClient;
 	int len = sizeof(SOCKADDR);
 
-	while (1)
+	std::cout << "Start TCP accept!!!!!!!!" << endl;
+	while (true)
 	{
 		SOCKET sockConn = accept(sockSrv, (SOCKADDR*)&addrClient, &len);
-		char sendBuf[256] = { 0 };
-		sprintf_s(sendBuf, 256, "Welcome %s!", inet_ntoa(addrClient.sin_addr));
-		send(sockConn, sendBuf, strlen(sendBuf) + 1, 0);
-		std::cout << sendBuf << "\n";
+		char cBuf[256] = { 0 };
+		sprintf_s(cBuf, 256, "%s", inet_ntoa(addrClient.sin_addr));
+		//send(sockConn, sendBuf, strlen(sendBuf) + 1, 0);
+		std::cout << "Hello IP=" << cBuf << std::endl;
 
-		char recvBuf[256];
-		recv(sockConn, recvBuf, 256, 0);
-		printf("%s\n", recvBuf);
-		closesocket(sockConn);
+		SThreadInfo* lpParam = new SThreadInfo();
+		lpParam->socket = sockConn;
+		lpParam->strIP = cBuf;
+		unsigned int nThreadId = 0;
+		HANDLE hThread = (HANDLE)_beginthreadex(NULL, 0, threadProc, (void*)lpParam, 0, &nThreadId);
 	}
-
-	//std::cout << "Hello World!\n"; 
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+unsigned __stdcall threadProc(LPVOID lpParam)
+{
+	SThreadInfo* pThreadInfo = (SThreadInfo*)lpParam;
+	SOCKET socket = pThreadInfo->socket;
+	char recvBuf[1024] = { 0 };
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	bool bExit = false;
+	while (!bExit)
+	{
+		int nRet = recv(socket, recvBuf, 1024, 0);
+		if (nRet > 0)
+		{
+			recvBuf[nRet] = '\0';
+			std::cout << "IP:" << pThreadInfo->strIP.c_str() << ",data=" << recvBuf << endl;
+			send(socket, recvBuf, strlen(recvBuf) + 1, 0);
+		}
+		else if (nRet == 0)
+		{
+			bExit = true;
+		}
+		else
+		{
+			std::cout << "Error!! Failed to recv, err=" << WSAGetLastError() << endl;
+			bExit = true;
+		}
+	}
+	std::cout << "IP:" << pThreadInfo->strIP.c_str() << " close!!!!!!" << endl;
+	closesocket(socket);
+	delete pThreadInfo;
+	return 0;
+}
