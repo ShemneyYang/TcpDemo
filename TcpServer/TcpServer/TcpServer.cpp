53d33dccb652b359e2bcf9866416ba6cb5f78370
yangquanyu <yangquanyu@yy.com>
2019-11-28 21:59:58 +0800

    tcpServerDemo

---
+++ TcpServer/TcpServer/TcpServer.cpp
@@ -0,0 +1,68 @@
+﻿// TcpServer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <Winsock2.h>
+#include <stdio.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+	wVersionRequested = MAKEWORD(1, 1);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		return -1;
+	}
+
+
+	if (LOBYTE(wsaData.wVersion) != 1 ||
+		HIBYTE(wsaData.wVersion) != 1) {
+		WSACleanup();
+		return -1;
+	}
+	SOCKET sockSrv = socket(AF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	addrSrv.sin_family = AF_INET;
+	addrSrv.sin_port = htons(8866);
+
+	bind(sockSrv, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
+
+	listen(sockSrv, 5);
+
+	SOCKADDR_IN addrClient;
+	int len = sizeof(SOCKADDR);
+
+	while (1)
+	{
+		SOCKET sockConn = accept(sockSrv, (SOCKADDR*)&addrClient, &len);
+		char sendBuf[256] = { 0 };
+		sprintf_s(sendBuf, 256, "Welcome %s!", inet_ntoa(addrClient.sin_addr));
+		send(sockConn, sendBuf, strlen(sendBuf) + 1, 0);
+		std::cout << sendBuf << "\n";
+
+		char recvBuf[256];
+		recv(sockConn, recvBuf, 256, 0);
+		printf("%s\n", recvBuf);
+		closesocket(sockConn);
+	}
+
+	//std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
