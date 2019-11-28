296057a9f3227e1a92eb7d338ece740d4887ba08
yangquanyu <yangquanyu@yy.com>
2019-11-28 22:02:41 +0800

    TcpClient demo

---
+++ TcpClient/TcpClient/TcpClient.cpp
@@ -0,0 +1,54 @@
+﻿// TcpClient.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <Winsock2.h>
+#include <stdio.h>
+#pragma comment(lib,"ws2_32.lib")
+
+void main()
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+	wVersionRequested = MAKEWORD(1, 1);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		return;
+	}
+
+
+	if (LOBYTE(wsaData.wVersion) != 1 ||
+		HIBYTE(wsaData.wVersion) != 1) {
+		WSACleanup();
+		return;
+	}
+	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	addrSrv.sin_family = AF_INET;
+	addrSrv.sin_port = htons(6000);
+	connect(sockClient, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
+
+	char recvBuf[100];
+	recv(sockClient, recvBuf, 100, 0);
+	printf("%s\n", recvBuf);
+	send(sockClient, "This is lisi", strlen("This is lisi") + 1, 0);
+
+	closesocket(sockClient);
+	WSACleanup();
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
